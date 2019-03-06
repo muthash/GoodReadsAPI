50dbf0aa116915da81ca58436ac5f6ab92a135eb
muthash <kathingo92@gmail.com>
2019-03-06 11:58:43 +0300

    [Bug] Fix test package backward importation error

---
+++ tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+from app import create_app
+
+
+app = create_app(config_name='testing')
+
+@app.cli.command()
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+if __name__ == '__main__':
+    test()
