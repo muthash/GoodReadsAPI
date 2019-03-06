375ee12f62e337191016469cd88f12c43e730d37
muthash <kathingo92@gmail.com>
2019-03-06 22:59:50 +0300

    [Feature] Update user testcases

--- tests/test_user_views.py
+++ tests/test_user_views.py
@@ -6,10 +6,9 @@ import unittest
 import json
 import datetime
 
-from app import create_app
+from app import create_app, db
 
 class AuthTestCase(unittest.TestCase):
-    """Base Test Case"""
     def setUp(self):
         """Set up test variables"""
         self.app = create_app(config_name="testing")
@@ -26,20 +25,29 @@ class AuthTestCase(unittest.TestCase):
         }
         self.header = {'Content-Type': 'application/json'}
 
+        with self.app.app_context():
+            db.session.close()
+            db.drop_all()
+            db.create_all()
+
     def test_registration(self):
-        """Test user registration works correcty."""
         res = self.client.post('/auth/register', headers=self.header, data=json.dumps(self.user_data))
         result = json.loads(res.data.decode())
         self.assertEqual(result['message'], "Account created successfully")
         self.assertEqual(res.status_code, 201)
 
     def test_login(self):
-        """Test user registration works correcty."""
         self.client.post('/auth/register', headers=self.header, data=json.dumps(self.login_data))
         res = self.client.post('/auth/login', headers=self.header, data=json.dumps(self.login_data))
         result = json.loads(res.data.decode())
         self.assertEqual(result['message'], "Login successfull")
         self.assertEqual(res.status_code, 200)
+    
+    def tearDown(self):
+        """teardown all initialized variables"""
+        with self.app.app_context():
+            db.session.remove()
+            db.drop_all()
 
 if __name__ == "__main__":
     unittest.main()
